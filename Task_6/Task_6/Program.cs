ac2f0ad947a186bdd0e16f82b1af3f580b1eb6bf
Юрій Поліщук <hdd911@mail.ru>
2016-09-20 00:39:03 +0300

    6

---
+++ Task_6/Task_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 400 == 0 || year % 100 > 0)
+                {
+                    Console.WriteLine("Високосний");
+                    return;
+                }
+            }
+            
+               Console.WriteLine("Не високосний");
+
+        }
+    }
+}
