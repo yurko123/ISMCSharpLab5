5f776acea4bfd95db6808a11ca888867dd79e710
Юрій Поліщук <hdd911@mail.ru>
2016-09-19 22:19:01 +0300

    Завдання 5

---
+++ Task_5/Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = n / 100, b = (n / 10) % 10;
+            n %= 10;
+            Console.WriteLine(b * 100 + a * 10 + n);
+
+        }
+    }
+}
