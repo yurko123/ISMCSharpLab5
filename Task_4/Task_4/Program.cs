1b353dcea4eb6f763b37cc847bd268da101f71f0
Юрій Поліщук <hdd911@mail.ru>
2016-09-19 22:04:42 +0300

    Завдання 4

---
+++ Task_4/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch = int.Parse(Console.ReadLine());
+            int a = ch / 100, b = (ch / 10) % 10;
+            ch %= 10;
+            ch = a * 100 + ch * 10 + b;
+            Console.WriteLine(ch);
+        }
+    }
+}
