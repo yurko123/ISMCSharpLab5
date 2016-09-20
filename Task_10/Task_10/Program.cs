98da7aa7c90a77df487647a635e2a79642c75b41
Юрій Поліщук <hdd911@mail.ru>
2016-09-20 20:04:55 +0300

    завдання 10

---
+++ Task_10/Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double res = 50;
+            for (int i = 49; i > 0; i--)
+            {
+                res = i + Math.Sqrt(res);
+               
+            }
+            Console.WriteLine(Math.Sqrt(res));
+        }
+    }
+}
